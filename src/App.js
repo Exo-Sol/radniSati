7999f8c8fc8d64ab8afe073e1bb93fbb2cdee99d
Exo Sol <1.petarzlatar@gmail.com>
2021-01-30 17:10:11 +0100

    tryin diffirent animations, next is adding salary per hour functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DateNav from "./components/DateNav";
 import NameJob from "./components/addRenameJob/NameJob";
+import { useSpring, animated } from "react-spring";
 
 function App() {
   const [jobName, setJobName] = useState(null);
   const [curJob, setCurJob] = useState(null);
   const [addJob, setAddJob] = useState(false);
 
+  //////////////////////initial animation
+  const springProps = useSpring({ opacity: addJob ? 0 : 1 });
+
   //////////////////////////////////////////////////////////////////
   const catchName = (name) => {
-    if (Array.isArray(jobName)) {
-      setJobName([...jobName, name]);
-    } else {
-      let x = [];
-      x.push(name);
-      setJobName(x);
+    ///first check if name is enetered
+    if (name.length > 0) {
+      if (Array.isArray(jobName)) {
+        setJobName([...jobName, name]);
+      } else {
+        let x = [];
+        x.push(name);
+        setJobName(x);
+      }
+      setCurJob(name);
+      setAddJob(false);
     }
-    setCurJob(name);
-    setAddJob(false);
   };
 
+  /////code for saving jobs from state in local storage
+
+  useEffect(() => {
+    if (jobName) {
+      localStorage.setItem("jobs", jobName);
+    }
+  }, [jobName]);
   //////////////////////////////////////////////////////////
 
   const addJobClick = () => {
@@ -27,18 +41,21 @@ function App() {
   };
 
   const deleteJobClick = () => {
-    if (jobName && jobName.length > 1) {
-      let filteredAry = jobName.filter((e) => e !== curJob);
-      setJobName(filteredAry);
-      setCurJob(filteredAry[0]);
-    } else {
-      setJobName(null);
-      setCurJob(null);
+    if (window.confirm(`Izbrisi ${curJob}?`)) {
+      if (jobName && jobName.length > 1) {
+        let filteredAry = jobName.filter((e) => e !== curJob);
+        setJobName(filteredAry);
+        setCurJob(filteredAry[0]);
+      } else {
+        setJobName(null);
+        setCurJob(null);
+      }
     }
   };
   ////////////////////////////////////////////////////////
   const catchData = (dateObj, workHours, startEndTime) => {
     const finalObj = {
+      job: curJob,
       month: dateObj.month,
       day: dateObj.day,
       dayName: dateObj.dayName,
@@ -59,10 +76,10 @@ function App() {
       console.log(typeof x);
 
       x.push(retrivedObj);
-      console.log(x); // odi ibacuje prvi key value samo
+
       x.push(finalObj);
       let objForSaving = JSON.stringify(x);
-      console.log(objForSaving);
+
       localStorage.setItem(dateObj.year, objForSaving);
     } else {
       let x = [];
@@ -95,28 +112,24 @@ function App() {
   //////////////////////////////////////////////////////////////////////////////////////
   const jobDisplay = (
     <div className="jobDispl">
-      {jobName && jobName.length > 1 ? (
+      {jobName && jobName.length > 1 && (
         <span className="material-icons jobArrow" onClick={backName}>
           arrow_back_ios
         </span>
-      ) : (
-        <span></span>
       )}
 
       <h4 id="jN">{curJob}</h4>
-      {jobName && jobName.length > 1 ? (
+      {jobName && jobName.length > 1 && (
         <span className="material-icons jobArrow" onClick={fowardName}>
           arrow_forward_ios
         </span>
-      ) : (
-        <span></span>
       )}
     </div>
   );
   ////////////////////////////////////////////////////////////////////////////
   return (
     <div className="App">
-      {jobName ? (
+      {jobName && (
         <div>
           <button id="addJob" onClick={addJobClick}>
             Dodaj posao
@@ -125,13 +138,15 @@ function App() {
             Izbrisi posao
           </button>
         </div>
-      ) : (
-        <div></div>
       )}
       {jobName ? jobDisplay : <NameJob catchName={catchName} />}
-      {addJob ? <NameJob catchName={catchName} /> : <div></div>}
+      {addJob && <NameJob catchName={catchName} />}
 
-      <DateNav catchData={catchData} />
+      {jobName && (
+        <animated.div style={springProps}>
+          <DateNav catchData={catchData} />
+        </animated.div>
+      )}
     </div>
   );
 }
